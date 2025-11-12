996666d3f9d7a3b87d9a9651d9249753b38f9759
Gabriel <gabrielsaraivasampaio13@gmail.com>
2025-11-12 17:44:02 -0300

    Atualização

--- Biblioteca-master/Biblioteca/model/Funcionario.java
+++ Biblioteca-master/Biblioteca/model/Funcionario.java
@@ -14,6 +14,11 @@ public class Funcionario implements Usuario {
     }
 
     @Override
+    public void separador(){
+        System.out.println("#####################");
+    }
+
+    @Override
     public String getNome(){
 
         return nome;
