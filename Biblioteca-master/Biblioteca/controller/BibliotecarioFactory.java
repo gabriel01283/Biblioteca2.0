025ebd39e33a1977343e8ee3595c048983238dc7
Gabriel <gabrielsaraivasampaio13@gmail.com>
2025-11-12 21:05:32 -0300

    Atualização2.0

--- Biblioteca-master/Biblioteca/controller/BibliotecarioFactory.java
+++
@@ -1,8 +0,0 @@
-public class BibliotecarioFactory extends UsuarioFactory{
-
-    @Override
-    public Usuario criarUsuario(String nome){
-
-        return new Bibliotecario (nome);
-    }
-}
--- Biblioteca-master/Biblioteca/controller/FuncionarioFactory.java
+++
@@ -1,8 +0,0 @@
-public class FuncionarioFactory extends UsuarioFactory{
-
-    @Override
-    public Usuario criarUsuario(String nome){
-
-        return new Funcionario(nome);
-    }
-}
